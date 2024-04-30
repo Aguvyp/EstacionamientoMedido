5715ba6cedc771d6489227a1692a2233f6c2598e
Augusto <augustojoaquinsacco@gmail.com>
2024-04-30 11:18:30 -0300

    Agregar archivos de proyecto.

---
+++ EstacionamientoMedido/Controladores/EstacionamientoController.cs
@@ -0,0 +1,24 @@
+﻿using EstacionamientoMedido.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstacionamientoMedido.Controladores
+{
+    public class EstacionamientoController
+    {  
+        Repositorio repo = new Repositorio();
+
+        public void GuardarEstacionamiento(Estacionamiento e)
+        {
+            repo.Estacionamientos.Add(e);
+        }
+
+        public List<Estacionamiento> ObtenerEstacionamiento(Estacionamiento e)
+        {
+            return repo.Estacionamientos;
+        }
+    }
+}
